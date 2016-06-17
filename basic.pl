3fb417dc4e53f2fb9966da7f0a8944953bd6b716
eelster <elad.elster@gmail.com>
2016-06-17 00:42:35 -0700

    added plugin support

--- basic.pl
+++ basic.pl
@@ -3,6 +3,13 @@
 # Script description
 # copyright
 
+BEGIN {
+    unshift(@INC,"some_path_i_need") if -e 'some_path_i_need';
+}
+
+use Getopt::Long;
+use Module::Pluggable require => 1, search_path => "my_script_plugins";
+
 my $scriptname  = $0;
 $scriptname =~ s|.*/||;
 my $HELP =  <<ENDHELP
@@ -23,10 +30,22 @@ my $HELP =  <<ENDHELP
 ENDHELP
 ;
 
-use Getopt::Long;
+
 my $debug = 0;
 GetOptions("debug"   => \$debug,
            "help"      => sub {print $HELP; exit(1)},
     );
 
+my @plugs = plugins();
 
+foreach my $p (@plugs) {
+    $p->pre_body($relevant_info)
+        if $p->can('pre_body');
+}
+
+# do something
+
+foreach my $p (@plugs) {
+    $p->post_body($relevant_info)
+        if $p->can('post_body');
+}
